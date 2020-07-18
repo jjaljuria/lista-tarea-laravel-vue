23a9d0fe123147fd96c2c4e4a2c84c0dd4236f2b
José Aljuria <josejavieral13@gmail.com>
2020-07-18 15:06:00 -0400

    migracion creacion pomodoros

---
+++ database/migrations/2020_07_18_180908_creacion_tabla_pomodoro.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreacionTablaPomodoro extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pomodoros', function (Blueprint $table) {
+            $table->id();
+            $table->integer('pomodorosActivos')->default(4);
+            $table->boolean('periodoDescanso')->default(false);
+            $table->integer('tiempoPomodoroActualEnSegundos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pomodoros');
+    }
+}
